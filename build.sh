3b5e9da0c4b3beb86e13de22378aad52a740b049
Gowri855 <gowriM@08052005@gmail.com>
2025-12-10 22:05:44 +0530

    Add makemigrations to build script

--- build.sh
+++ build.sh
@@ -2,13 +2,21 @@
 # exit on error
 set -o errexit
 
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
+
 
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+
+echo "Creating migrations..."
+python manage.py makemigrations
+
+
 echo "Running migrations..."
 python manage.py migrate
+
 
 echo "Build completed successfully!"
